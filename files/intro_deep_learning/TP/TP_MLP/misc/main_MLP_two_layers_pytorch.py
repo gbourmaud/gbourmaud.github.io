74873301c8e5a4360e8ec77d6140ba1308154b27
BOURMAUD Guillaume <Guillaume.Bourmaud@bordeaux-inp.fr>
2025-02-07 11:53:44 +0100

    add MLP Pytorch jouet w\o autograd

--- files/intro_deep_learning/TP/TP_MLP/misc/main_MLP_two_layers_pytorch.py
+++ files/intro_deep_learning/TP/TP_MLP/misc/main_MLP_two_layers_pytorch.py
@@ -7,19 +7,19 @@ Created on Tue Mar 12 11:22:25 2019
 @author: guillaume
 """
 import matplotlib.pyplot as plt
-import numpy as np
-import sys
 import utils
+import torch as t
+import math 
 
-np.random.seed(0)
+t.manual_seed(0)
 
 #%% DEFINE AND PLOT DATA
     
 style_per_class = ['xb', 'or', 'sg']
-X = np.array([[1.2, 2.3, -0.7, 3.2, -1.3],[-3.4, 2.8, 1.2, -0.4, -2.3]]).T
+X = t.tensor([[1.2, 2.3, -0.7, 3.2, -1.3],[-3.4, 2.8, 1.2, -0.4, -2.3]]).T
 X -= X.mean() #centering data (globally)
 X /= X.std() #reduce data (globally)
-y = np.array([0,0,1,1,2])
+y = t.tensor([0,0,1,1,2])
 
 C = len(style_per_class)
 N = X.shape[0]
@@ -39,21 +39,21 @@ plt.pause(0.1)
    
         
 def FC_forward(X,W,b):
-    Z = X.dot(W) + b #NxH
+    Z = X.matmul(W) + b #NxH
     return Z
 
 def FC_backward(dc_dZ, X, W, b):
-    dc_dX = dc_dZ.dot(W.T)
-    dc_dW = X.T.dot(dc_dZ)
+    dc_dX = dc_dZ.matmul(W.T)
+    dc_dW = X.T.matmul(dc_dZ)
     dc_db = dc_dZ.sum(axis=0)
     return dc_dX, dc_dW, dc_db
 
 def relu_forward(X):
-    Z = np.maximum(0.,X)
+    Z = t.maximum(t.tensor(0.),X)
     return Z
 
 def relu_backward(dc_dZ, X):
-    dc_dX = dc_dZ.copy()
+    dc_dX = dc_dZ.clone()
     dc_dX[X<0.] = 0.
     return dc_dX
     
@@ -66,16 +66,16 @@ class MLP:
         
 
         #parameters
-        self.W1 = (np.sqrt(6./self.D))*(2*(np.random.uniform(size=(self.D,self.H))-0.5))
-        self.b1 = (1./np.sqrt(self.D))*(2*(np.random.uniform(size=(self.H))-0.5))
-        self.W3 = (np.sqrt(6./self.H))*(2*(np.random.uniform(size=(self.H,self.C))-0.5))
-        self.b3 = (1./np.sqrt(self.H))*(2*(np.random.uniform(size=(self.C))-0.5))
+        self.W1 = (math.sqrt(6./self.D))*(2*(t.rand(size=(self.D,self.H))-0.5))
+        self.b1 = (1./math.sqrt(self.D))*(2*(t.rand(size=(self.H,))-0.5))
+        self.W3 = (math.sqrt(6./self.H))*(2*(t.rand(size=(self.H,self.C))-0.5))
+        self.b3 = (1./math.sqrt(self.H))*(2*(t.rand(size=(self.C,))-0.5))
         
         #gradients
-        self.dc_dW1 = np.zeros_like(self.W1)
-        self.dc_db1 = np.zeros_like(self.b1)
-        self.dc_dW3 = np.zeros_like(self.W3)
-        self.dc_db3 = np.zeros_like(self.b3)
+        self.dc_dW1 = t.zeros_like(self.W1)
+        self.dc_db1 = t.zeros_like(self.b1)
+        self.dc_dW3 = t.zeros_like(self.W3)
+        self.dc_db3 = t.zeros_like(self.b3)
         
 
         
@@ -112,10 +112,10 @@ class GradientDescentWithMomentum:
         self.lr = lr
         
         #momentum
-        self.VW1 = np.zeros_like(self.model.W1)
-        self.Vb1 = np.zeros_like(self.model.b1)
-        self.VW3 = np.zeros_like(self.model.W3)
-        self.Vb3 = np.zeros_like(self.model.b3)
+        self.VW1 = t.zeros_like(self.model.W1)
+        self.Vb1 = t.zeros_like(self.model.b1)
+        self.VW3 = t.zeros_like(self.model.W3)
+        self.Vb3 = t.zeros_like(self.model.b3)
         
     def step(self):
         self.VW1 = self.beta*self.VW1 + (1.0-self.beta)*self.model.dc_dW1
@@ -131,30 +131,30 @@ class GradientDescentWithMomentum:
         self.model.b3 -= self.lr*self.Vb3
     
     def zero_gradients(self):
-        self.model.dc_dW1.fill(0.)
-        self.model.dc_db1.fill(0.)
-        self.model.dc_dW3.fill(0.)
-        self.model.dc_db3.fill(0.)
+        self.model.dc_dW1.zero_()
+        self.model.dc_db1.zero_()
+        self.model.dc_dW3.zero_()
+        self.model.dc_db3.zero_()
         
     
 def logsoftmax(x):
-    x_shift = x - np.amax(x, axis=1, keepdims=True)
-    return x_shift - np.log(np.exp(x_shift).sum(axis=1, keepdims=True))   
+    x_shift = x - t.amax(x, axis=1, keepdims=True)
+    return x_shift - t.log(t.exp(x_shift).sum(axis=1, keepdims=True))   
     
 def softmax(x):
-    e_x = np.exp(x - np.amax(x, axis=1, keepdims=True))
+    e_x = t.exp(x - t.amax(x, axis=1, keepdims=True))
     return e_x / e_x.sum(axis=1, keepdims=True)
     
 def crossEntropyLoss(S, y):
     N = y.shape[0]
-    P = softmax(S.astype('double'))
-    log_p = logsoftmax(S.astype('double'))
-    a = log_p[np.arange(N),y]
+    P = softmax(S.to(t.float64))
+    log_p = logsoftmax(S.to(t.float64))
+    a = log_p[t.arange(N),y]
     l = -a.sum()/N
     dc_dS = P
-    dc_dS[np.arange(N),y] -= 1
+    dc_dS[t.arange(N),y] -= 1
     dc_dS = dc_dS/N
-    return (l, dc_dS)
+    return (l.to(t.float32), dc_dS.to(t.float32))
         
 
 #%% FIGURES
@@ -183,8 +183,8 @@ while 1:
     [c, dc_dS] = crossEntropyLoss(S, y)
     
     #Print Loss and Classif Accuracy
-    pred = np.argmax(S, axis=1)
-    acc = (np.argmax(S, axis=1) == y).astype('float').sum()/N
+    pred = t.argmax(S, axis=1)
+    acc = (t.argmax(S, axis=1) == y).to(t.float32).sum()/N
     print('Iter {} | Training Loss = {} | Training Accuracy = {}%'.format(it,c,acc*100))
 
     #Backward Pass (Compute Gradient)
@@ -197,7 +197,7 @@ while 1:
     
     c_seq.append(c)
     it_seq.append(it)
-    if(np.mod(it,10)==0):
+    if(it%10==0):
         #Plot decision boundary
         axs1[0].cla()
         for i in range(C):
